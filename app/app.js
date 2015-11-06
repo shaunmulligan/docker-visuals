b78899f253ab0e17bd3ad09d499007f0adc5189c
Craig Mulligan <craig@firingsuad.co.za>
2015-11-06 13:19:50 +0000

    changes to test push

--- app/app.js
+++ app/app.js
@@ -1,15 +1,32 @@
 var app = angular.module('myApp', []);
 var resin = require("resin-sdk");
 
-function terminalCtrl($scope, terminalService)
-{
-    $scope.terminal = terminalService.tty();
+function terminalCtrl($scope)
+{ 
+    $scope.tty = 'output'
+    var spawn = require('child_process').spawn;
+    var command = spawn('git', ['push', 'resin', 'master', '--force']);
+    var result = '';
+
+    command.stderr.on('data', function(data) {
+    	result += data + "</br>"
+        $scope.$apply(function(){
+	      $scope.tty = result;
+	    });
+    });
+    command.on('error', function () {
+	  console.log("Failed to start child.");
+	});
+    command.on('close', function(code) {
+        console.log("callback")
+    });
+
 }
 
 
-app.controller('devicesCtrl', function( devicesService, $scope) {
-  $scope.devices = devicesService.data;
-  console.log($scope.devices)
-  // console.log(devicesService.data)
+// app.controller('devicesCtrl', function( devicesService, $scope) {
+//   $scope.devices = devicesService.data;
+//   // console.log($scope.devices)
+//   // console.log(devicesService.data)
 
-});
+// });
--- app/services.js
+++ app/services.js
@@ -10,32 +10,53 @@ resin.auth.login(credentials, function(error) {
   console.log("success")
 });
 
-app.service('terminalService', function(){
-    this.tty = function() {
-		// return childProcess.exec('ls -l', function (error, stdout, stderr) {
-		// 	console.log(stdout)
-		//   	return stdout;
-		// });
-		return "Terminal output should go here"
-    }
+function run(cmd, callback) {
+    
+}
+
+function close(){
+	console.log('tty process fin')
+}
+
+app.service('terminalService', function($rootScope){
+	 this.data = 0
+    console.log()
+
+	var spawn = require('child_process').spawn;
+    var command = spawn("ls");
+    var result = '';
+    command.stdout.on('data', function(data) {
+         result += data.toString();
+         
+    });
+    command.on('close', function(code) {
+        console.log("callback")
+	        this.data = result;
+	        this.data = 1;
+	        console.log(this.data)
+	        $rootScope.$apply();
+    });
+	
 });
 
 
-app.factory('devicesService', function($timeout) {
-	var data = {};
-    (function tick() {
-        resin.models.device.getAll(function(error, devices) {
-		  if (error != null) {
-		    throw error;
-		  }
-		  data.resp = devices;
-		  // console.log(devices)
-		});  //1. this returns promise
 
-	$timeout(tick, 1000);
-  	})();
 
-  	return {
-	   data: data
-	};
-});
+// app.factory('devicesService', function($timeout) {
+// 	var data = {};
+//     (function tick() {
+//         resin.models.device.getAll(function(error, devices) {
+// 		  if (error != null) {
+// 		    throw error;
+// 		  }
+// 		  data.resp = devices;
+// 		  // console.log(devices)
+// 		});  //1. this returns promise
+
+// 	$timeout(tick, 1000);
+//   	})();
+
+//   	return {
+// 	   data: data
+// 	};
+// });
