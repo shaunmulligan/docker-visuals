09b93eeb22320163813402d17a984cfd448b67dc
Craig Mulligan <craig@firingsuad.co.za>
2015-11-10 13:18:31 +0000

    kiosk mode"

--- main.js
+++ main.js
@@ -22,14 +22,14 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   // Create the browser window.
   // mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow = new BrowserWindow({fullscreen: true});
+  mainWindow = new BrowserWindow({fullscreen: true, auto-hide-menu-bar: true});
   // and load the index.html of the app.
   // mainWindow.loadUrl('file://' + __dirname + '/app/splash.html');
 
   mainWindow.loadUrl('file://' + __dirname + '/src/index.html');
 
   // Open the DevTools.
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
