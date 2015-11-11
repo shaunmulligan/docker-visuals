b08374a9faf64315b9b9dc7f5b759e8decb9ddce
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-11-11 23:00:56 +0000

    fix up path

--- init.sh
+++ init.sh
@@ -8,13 +8,12 @@ eval "$(ssh-agent -s)"
 echo $KEY > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 
-ssh-add /data/id_rsa
+ssh-add /root/.ssh/id_rsa
 
 git config --global user.email $EMAIL
 git config --global user.name $NAME
 
 if [ "$ERASE" == "TRUE" ]; then
-
 	echo "Erasing project"
 	cd /data
 	rm -rf applause-o-meter
